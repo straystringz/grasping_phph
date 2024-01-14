d0564f1f1e429a91748aa8816e283d8627c7d6e8
Qrued Resources <victor.j.emman@gmail.com>
2024-01-14 19:59:26 +0100

    I want to check how much of PHP I can grasp in a little amount of time

---
+++ assarr.php
@@ -0,0 +1,10 @@
+<?php
+$prices = [ 
+	'Laptop' => 3500,
+	'Server' => 3_000_000,
+	'Keyboard and Mouse' => 50
+];
+
+echo  $prices['Laptop'] . PHP_EOL;
+echo $prices['Keyboard and Mouse'] . PHP_EOL;
+echo $prices['Server'] . PHP_EOL;
